4507e82f3fc98fd604f4ca4af8271fcde8a1a50c
shantanu gondane <shantanugondane44@gmail.com>
2022-11-12 13:52:38 +0530

    changes made

--- src/App.js
+++ src/App.js
@@ -38,6 +38,7 @@ class App extends Component {
                   <CovidApp
                     setDarkMode={this.setDarkMode}
                     isDarkMode={this.state.isDarkMode}
+                    // ssds
                   />
                 )}
               />
